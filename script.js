548e6e05a7a0579cfe2cafacfb4205ef20c0c9ec
AF072897 <Alex.Fitzmaurice@cerner.com>
2020-04-17 11:01:36 -0500

    Added token classes/images

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-  
-   //   Final Project
+//   Final Project
 
-   //   Author: Ashley D, Alex F, and Matthew K
-    //  Date:   4-15-2020
+//   Author: Ashley D, Alex F, and Matthew K
+//   Date:   4-15-2020
 
-    //  Filename: script.js
- 
+//   Filename: script.js
+
+"use strict";
+
+var x = {
+   player = 1,
+   playerName = "",
+   icon = document.getElementById("img").getAttribute("src")
+};
+
+var o = {
+   player = 2,
+   playerName = "",
+   icon = document.getElementById("img").getAttribute("src")
+};
