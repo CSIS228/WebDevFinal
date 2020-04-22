60b4ca6c351a9cba65a7fbe9bacac16a0df8c12e
AF072897 <Alex.Fitzmaurice@cerner.com>
2020-04-22 17:02:57 -0500

    Added main functions and updated comments

--- script.js
+++ script.js
@@ -7,14 +7,48 @@
 
 "use strict";
 
+//global player variables which hold player number, name, and icon
 var x = {
-   player = 1,
-   playerName = "",
-   icon = document.getElementById("img").getAttribute("src")
+   player: 1,
+   playerName: "",
+   //HTML doesn't have an image object yet, will implement this later
+   // icon: document.getElementById("img").getAttribute("src")
 };
 
 var o = {
-   player = 2,
-   playerName = "",
-   icon = document.getElementById("img").getAttribute("src")
+   player: 2,
+   playerName: "",
+   //HTML doesn't have an image object yet, will implement this later
+   // icon: document.getElementById("img").getAttribute("src")
 };
+
+//Prompts user in window for a name
+function getPlayerName(player) {
+   player.playerName = window.prompt("Please enter your name here:");
+}
+
+function createPlayers() {
+   //create this when method for choosing 2players or Ai is known
+   getPlayerName(x);
+   getPlayerName(o);
+}
+
+function updateBoard() {
+   //main board update function. should use several helper functions to grab info
+   createPlayers();
+   window.alert(x.playerName);
+   window.alert(o.playerName);
+}
+
+function createEventListeners() {
+   //add event listeners here
+}
+
+//using this to test functions as they progress for now, will update
+//this when project is close to complete
+if (window.addEventListener) {
+   window.addEventListener("load", updateBoard(), false);
+}
+else if (window.attachEvent) {
+   window.attachEvent("onload", updateBoard());
+}
