78cf2042513fc5b7beb443dc529eb532a88101df
AF072897 <Alex.Fitzmaurice@cerner.com>
2020-04-23 11:38:39 -0500

    Created turn-based logic and updated player selection logic

--- script.js
+++ script.js
@@ -14,30 +14,55 @@ var x = {
    //HTML doesn't have an image object yet, will implement this later
    // icon: document.getElementById("img").getAttribute("src")
 };
-
 var o = {
    player: 2,
    playerName: "",
    //HTML doesn't have an image object yet, will implement this later
    // icon: document.getElementById("img").getAttribute("src")
 };
+var turnCount = 0;
+var currentPlayer = "";
 
 //Prompts user in window for a name
-function getPlayerName(player) {
-   player.playerName = window.prompt("Please enter your name here:");
+function getPlayerName(mode) {
+   switch (mode) {
+      case "AI":
+         x.playerName = window.prompt("Player 1, please enter your name here: ");
+         o.playerName = "Computer";
+         break;
+      case "2P":
+         x.playerName = window.prompt("Player 1, please enter your name here: ");
+         o.playerName = window.prompt("Player 2, please enter your name here: ");
+         break;
+      default:
+         window.alert("Error");
+   }
 }
 
 function createPlayers() {
    //create this when method for choosing 2players or Ai is known
-   getPlayerName(x);
-   getPlayerName(o);
+   getPlayerName("2P");
+}
+
+function incrementTurn() {
+   turnCount++;
+   if (turnCount % 2 == 0) {
+      currentPlayer = o.playerName;
+      return currentPlayer;
+   }
+   currentPlayer = x.playerName;
+   return currentPlayer;
 }
 
 function updateBoard() {
    //main board update function. should use several helper functions to grab info
    createPlayers();
-   window.alert(x.playerName);
-   window.alert(o.playerName);
+   for (var turn = 0; turn < 5; turn++) {
+      incrementTurn();
+      window.alert(currentPlayer);
+      incrementTurn();
+      window.alert(currentPlayer);
+   }
 }
 
 function createEventListeners() {
